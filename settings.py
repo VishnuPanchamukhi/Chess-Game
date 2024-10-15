acae82f8a838406ba370c25e537c344df5dd17d1
VishnuPanchamukhi <166757089+VishnuPanchamukhi@users.noreply.github.com>
2024-10-15 20:44:37 +0100

    Update settings.py
    - Added the time module
    - Added variables to track time

--- settings.py
+++ settings.py
@@ -5,13 +5,21 @@ import copy
 import random
 import numpy as np
 from openingBook import *
+import time
 
 # sceen dimensions
 
-TILESIZE = 64
-WIDTH = TILESIZE * 8
-HEIGHT = TILESIZE * 8
+TILESIZE = 73
+WIDTH = TILESIZE * 10.5
+HEIGHT = TILESIZE * 10.5
 FPS = 60
+excessWidth = (WIDTH - (TILESIZE * 8)) / 2
+excessHeight = (HEIGHT - (TILESIZE * 8)) / 2
+
+# time controls
+
+PLAYERTIME = 3600
+BOTTIME = 3600
 
 # colours
 
@@ -22,10 +30,12 @@ YELLOW = (255, 255, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 GREY = (30, 30, 30)
+BG = (49, 46, 43)
+TIMERCOLOUR = (40, 41, 42)
 
 # board array
 
-board = [['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
+board = [['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],  # uppercase --> black and lowercase --> white
          ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
          ['',  '',  '',  '',  '',  '',  '',  ''], 
          ['',  '',  '',  '',  '',  '',  '',  ''], 
@@ -50,7 +60,13 @@ wR = pg.transform.scale(pg.image.load('img/wRook.png'), (TILESIZE, TILESIZE))
 wN = pg.transform.scale(pg.image.load('img/wKnight.png'), (TILESIZE, TILESIZE))
 
 lightSquare = pg.transform.scale(pg.image.load('img/lightSquare.png'), (TILESIZE, TILESIZE))
-darkSquare = pg.transform.scale(pg.image.load('img/darkSquare.png'), (WIDTH, HEIGHT))
+darkSquare = pg.transform.scale(pg.image.load('img/darkSquare.png'), (TILESIZE * 8, TILESIZE * 8))
 highlightSquare = pg.transform.scale(pg.image.load('img/highlight.png'), (TILESIZE, TILESIZE))
 highlightSquare2 = pg.transform.scale(pg.image.load('img/highlight2.png'), (TILESIZE, TILESIZE))
 check = pg.transform.scale(pg.image.load('img/check.png'), (TILESIZE, TILESIZE))
+bgColour = pg.transform.scale(pg.image.load('img/bgColour.png'), (WIDTH, HEIGHT))
+
+
+# dictionary to help with scoring
+
+pieceValues = {'p':100, 'n':324, 'b':340, 'r':500, 'q':900, 'k':0}
